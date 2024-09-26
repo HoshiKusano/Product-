bce4f2f58c088aea4102a8c34dfa1509d492ea00
HoshiKusano <bb48122030@ms.nagasaki-u.ac.jp>
2024-09-27 02:13:38 +0900

    PAGINATE

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,24 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-       if(env('APP_ENV') !== 'local') {
-        \URL::forceScheme('https');  //
+       paginator::useBootstrap();
+       
+       
+        // Paginator::useBootstrapFive();    公式ドキュメント
+       //または Paginator::useBootstrapFour();    公式ドキュメン
     }
-}
 }
