af2a0f021694015f39032a79a0eaf3fbb0bb7966
HoshiKusano <bb48122030@ms.nagasaki-u.ac.jp>
2024-10-04 19:41:33 +0900

    Create_Post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts" method="post">
+           @csrf
+           <div class="title">
+               <h2>Tilte</h2>
+               <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+               <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+           </div>
+           <div class="body">
+               <h2>Body</h2>
+               <textarea name="post[body]" placeholder="本文を書く場所です。" >{{ old('post.body') }}</textarea>
+               <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
+           </div>
+           <input type="submit" value="store"/>
+        </form>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
