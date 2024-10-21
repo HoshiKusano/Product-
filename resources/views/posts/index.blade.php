58800463fa5a4d5a1ab90224bf6f82d817489ec6
HoshiKusano <bb48122030@ms.nagasaki-u.ac.jp>
2024-10-21 23:02:28 +0900

    API入れてみた

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,4 +30,14 @@
                 }
             }
         </script>
+        
+        <div>
+            @foreach($questions as $question)
+            <div>
+                <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                {{ $question['title'] }}
+                </a>
+            </div>
+            @endforeach
+        </div>
 </x-app-layout>
